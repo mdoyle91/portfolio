01946de43a1f5ca81e42fc5fdbd77e58c615b16b
mdoyle91 <mike.w.doyle91@gmail.com>
2025-03-04 15:07:14 -0500

    Navbar w/ GH and LI Icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,22 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Projects from "./components/Projects/Projects.jsx";
-// import { useState } from "react";  From review of changing state--not sure that will be needed for the creation of this portfolio
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
-const projects = [
-  {
-    name: "Project 4",
-    type: "Django",
-    id: 1,
-  },
-  {
-    name: "Project 2",
-    type: "Express",
-    id: 2,
-  },
-];
-
-const navLinks = [
-  { href: "/", text: "Home" },
-  { href: "/about", text: "About" },
-  { href: "/projects", text: "Projects" },
-  { href: "/testimonials", text: "Testimonials" },
-  { href: "/contact", text: "Contact" },
-];
-
 const App = () => {
-  // const [isDarkMode, setIsDarkMode] = useState(false); From review of changing state--not sure that will be needed for the creation of this portfolio
-
   return (
     <>
-      {/* <div className={isDarkMode ? "dark" : "light"}> From review of changing state--not sure that will be needed for the creation of this portfolio*/}
-      <Navbar links={navLinks} />
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
-      <h2>Projects</h2>
-      <ul>
-        {projects.map((project) => (
-          <Projects key={project.id} {...project} /> //Not sure if I want to use the spread operator  here or project={project}
-        ))}
-      </ul>
-      {/* </div> */}
+      <Navbar />
+      {/* May want to use links={navLinks} and the NavbarLink.jsx page per React Components, Nested Components section*/}
     </>
   );
 };
 export default App;
+
+// const navLinks = [  Will go into the return statement
+//   { href: "/", text: "Home" },
+//   { href: "/about", text: "About" },
+//   { href: "/projects", text: "Projects" },
+//   { href: "/testimonials", text: "Testimonials" },
+//   { href: "/contact", text: "Contact" },
+// ];
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,33 @@
+import { Link } from "react-router-dom";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./Navbar.css";
 import NavbarLink from "../NavbarLink/NavbarLink.jsx";
 //WilL probably want to use react-router-dom for this as it's a SPA. Checkout React-Router's Navlink function--more info on React Router DOM-Creating Links
 
 const Navbar = ({ links }) => {
   return (
-    <nav id="top-navbar">
-      {links.map((link, index) => (
-        <NavbarLink key={index} href={link.href} text={link.text} /> //Having my NavBar coded as such, rather than hard coded, will allow me to add icons/individually style each item of the Navbar.
-      ))}
-    </nav>
+    <>
+      <nav>
+        <h3>Mike Doyle</h3>
+        <h3>Full-Stack Developer</h3>
+        <Link to="https://github.com/mdoyle91">
+          <FaGithub />
+          GitHub
+        </Link>
+        <Link to="https://www.linkedin.com/in/michaeldoyle91/">
+          <FaLinkedin />
+          LinkedIn
+        </Link>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/testimonials">Testimonials</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </>
   );
 };
 
 export default Navbar;
+
+// <NavbarLink key={index} href={link.href} text={link.text} /> Having my NavBar coded as such, rather than hard coded, will allow me to add icons/individually style each item of the Navbar.
